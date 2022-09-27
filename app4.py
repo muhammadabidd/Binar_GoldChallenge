067a85fdd95ed2df3fcec5a1e24a16782a5a553a
muhammadabidd <muhammmadabidd@gmail.com>
2022-09-28 00:21:19 +0700

    Succes

--- app4.py
+++ app4.py
@@ -1,4 +1,5 @@
 import re
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -7,7 +8,7 @@ from flask import request
 from flasgger import Swagger, LazyString, LazyJSONEncoder
 from flasgger import swag_from
 
-app.json_encoder = LazyJSONEncoder
+app.json_encoder= LazyJSONEncoder
 swagger_template = dict(
 info = {
     'title': LazyString(lambda : 'API Documentation for Data Processing and Modeling'),
@@ -31,7 +32,6 @@ swagger_config = {
 }
 swagger = Swagger(app, template=swagger_template, config = swagger_config)
 
-
 @swag_from("docs/hello_world.yml", methods = ['GET'])
 @app.route('/', methods=['GET'])
 def hello_world():
@@ -44,8 +44,7 @@ def hello_world():
     response_data = jsonify(json_response)
     return response_data
 
-
-@swag_from("/docs/hello_world.yml", methods = ['GET'])
+@swag_from("docs/hello_world.yml", methods = ['GET'])
 @app.route('/text', methods=['GET'])
 def text():
     json_response = {
@@ -57,7 +56,6 @@ def text():
     response_data = jsonify(json_response)
     return response_data
 
-
 @swag_from("docs/hello_world.yml", methods = ['POST'])
 @app.route('/text_clean', methods=['POST'])
 def text_clean():
@@ -66,25 +64,25 @@ def text_clean():
         'description' : "Original Teks",
         'data' : re.sub(r'[^a-zA-Z0-9]', ' ', "Halo, apa kabar semua?)(*)(*()*&"),
     }
-
+    
     response_data = jsonify(json_response)
     return response_data
 
-@swag_from("docs/text_processing.yml", methods=['POST'])
-@app.route('/text-processing', methods=['POST'])
+
+@swag_from("docs/text_processing.yml", methods = ['POST'])
+@app.route('/text_processing', methods=['POST'])
 def text_processing():
 
     text = request.form.get('text')
-    
+
     json_response = {
         'status_code' : 200,
-        'description' : "Teks yang sudah di Proses",
+        'description' : "Teks yang sudah diproses",
         'data' : re.sub(r'[^a-zA-Z0-9]', ' ', text),
     }
 
     response_data = jsonify(json_response)
     return response_data
-
 
 
 if __name__ == '__main__' :
