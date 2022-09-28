b03386756cc0f6f9b708dff6131a8f8ec8ecfb02
muhammadabidd <muhammmadabidd@gmail.com>
2022-09-28 22:59:27 +0700

    asdasd

--- app4.py
+++ app4.py
@@ -110,6 +110,7 @@ def upload_csv():
    if request.method == 'POST':
         file = request.files['file']
 <<<<<<< HEAD
+<<<<<<< HEAD
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
@@ -120,6 +121,13 @@ def upload_csv():
 
         file_clean = re.sub(r'[^a-zA-Z0-9]','',data)
 
+=======
+             
+        data = pd.read_csv(file)
+
+        file_clean = re.sub(r'[^a-zA-Z0-9]','',data)
+
+>>>>>>> parent of f77c729 (Succes make Function)
 =======
              
         data = pd.read_csv(file)
