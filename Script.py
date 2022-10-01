66db94bda912b361f9d2f9ceabc79950decec364
muhammadabidd <muhammmadabidd@gmail.com>
2022-10-01 10:10:33 +0700

    Configurating app4 and script
    still cannot figure out how to connect between variabel

--- Script.py
+++ Script.py
@@ -1,2 +1,54 @@
 import re
 import pandas as pd
+from werkzeug.utils import secure_filename
+import os
+from flask import request
+
+
+
+def lowercase(text):
+    new_text = text.lower()
+    return new_text
+
+
+def remove_unnecessary_char(text):
+    text = re.sub(r'\\n',' ', text)
+    text = re.sub('rt','', text)
+    text = re.sub('user','', text)
+    text = re.sub(';','', text)
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))',' ',text)
+    text = re.sub('  +',' ', text)
+    text = re.sub('\d+\.\s',' ', text)
+    text = re.sub(r"x[a-z0-9][a-z0-9]",'',text)
+    return text
+
+
+def remove_nonaplhanumeric(text):
+    text = re.sub(r'[^0-9a-zA-Z\?!]+', ' ', text)
+    text = re.sub("\s\s+" , " ", text)
+    text = re.sub('^\s','', text)
+    return text
+
+
+def remove_duplicateexclamation(text):
+    text = re.sub(r'[!]{2,}', '!', text)
+    text = re.sub(r'[?]{2,}', '?', text)
+    return text
+
+
+kamus_alay = pd.read_csv('Helper/kamusalay.csv')
+alay_dict_map = dict(zip(kamus_alay['alay'], kamus_alay['arti'])) 
+def normalize_alay(text):
+    for word in alay_dict_map:
+        normalized_word = ' '.join([alay_dict_map[word] if word in alay_dict_map else word for word in text.split(' ')])
+        return normalized_word
+
+
+
+def process_word(text):
+    for text in first_column_pre_process:
+        text = lowercase(text)
+        text = remove_unnecessary_char(text)
+        text = remove_nonaplhanumeric(text)
+        text = remove_duplicateexclamation(text)
+        text = normalize_alay(text)
