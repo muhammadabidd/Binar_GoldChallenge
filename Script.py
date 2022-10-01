03d56b81358db9918b36bec42eb7696c0b105e42
muhammadabidd <muhammmadabidd@gmail.com>
2022-10-01 11:58:10 +0700

    Tidy App Folder

--- Script.py
+++ Script.py
@@ -1,14 +1,15 @@
+#Importing libraries
 import re
 import pandas as pd
 
 
-
-
+#Func for lowering text
 def lowercase(text):
     new_text = text.lower()
     return new_text
 
 
+#Func for remove unnecessary_char
 def remove_unnecessary_char(text):
     text = re.sub(r'\\n',' ', text)
     text = re.sub('rt','', text)
@@ -21,6 +22,7 @@ def remove_unnecessary_char(text):
     return text
 
 
+#Func for remove nonalphanumeric
 def remove_nonaplhanumeric(text):
     text = re.sub(r'[^0-9a-zA-Z\?!]+', ' ', text)
     text = re.sub("\s\s+" , " ", text)
@@ -28,6 +30,7 @@ def remove_nonaplhanumeric(text):
     return text
 
 
+#Func for remove duplicate exclamation mark
 def remove_duplicateexclamation(text):
     text = re.sub(r'[!]{2,}', '!', text)
     text = re.sub(r'[?]{2,}', '?', text)
@@ -36,7 +39,8 @@ def remove_duplicateexclamation(text):
     return text
 
 
-kamus_alay = pd.read_csv('Helper/kamusalay.csv')
+#Func to normalize "alay" word to formal word
+kamus_alay = pd.read_csv('Source/kamusalay.csv')
 alay_dict_map = dict(zip(kamus_alay['alay'], kamus_alay['arti'])) 
 def normalize_alay(text):
     for word in alay_dict_map:
@@ -44,7 +48,7 @@ def normalize_alay(text):
         return normalized_word
 
 
-
+#Final function
 def process_word(text):
     text = lowercase(text)
     text = remove_unnecessary_char(text)
